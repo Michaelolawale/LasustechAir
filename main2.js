fb93f67c0603ac7273d67d756fb21481b02fdf19
MichaelOlawale <olawalemichael335@gmail.com>
2024-08-23 12:55:49 +0100

    make

--- main2.js
+++
@@ -1,9 +0,0 @@
-import AOS from './node_modules/aos/dist/aos.js';
-import './node_modules/aos/dist/aos.css';
-
-// Initialize AOS with custom options
-AOS.init({
-    useClassNames: true,
-    initClassName: false,
-    animatedClassName: 'animated',
-});
